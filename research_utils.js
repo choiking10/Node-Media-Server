6b46a630defb45d6a13c88d951be4e33b285c409
Yunho Choi <choiking10@kaist.ac.kr>
2020-03-24 17:23:23 +0900

    Fix bug

--- research_utils.js
+++ research_utils.js
@@ -15,7 +15,7 @@ function getTimestamp() {
 
 function getFileTimestamp() {
     let ptime = new Date(Date.now());
-    return [ptime.getMonth(), ptime.getDate(), '_', ptime.getHours(), ptime.getMinutes()].join();
+    return [ptime.getMonth(), ptime.getDate(), '_', ptime.getHours(), ptime.getMinutes()].join('');
 }
 
 const START_TIMESTAMP = getFileTimestamp();
