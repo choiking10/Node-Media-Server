4321c560bf45f12fe4bafaeed1c27a80a00dee62
Yunho Choi <choiking10@kaist.ac.kr>
2020-05-18 10:41:27 +0900

    update set interval

--- black_client_just_receive.js
+++ black_client_just_receive.js
@@ -1,5 +1,5 @@
 
-let LOCAL_TEST = true;
+let LOCAL_TEST = false;
 
 let EDGE_JUPITER_IP = "10.0.10.1";
 let EDGE_JUPITER_PORT = 1935;
@@ -13,19 +13,19 @@ if(LOCAL_TEST) {
 const NodeRtmpEdgeChangeClient = require('./node_rtmp_edge_change_client');
 const stream_key = "wins2";
 
-let pull_jupiter = new NodeRtmpEdgeChangeClient(
-    'rtmp://' + EDGE_JUPITER_IP + '/live/' + stream_key
-);
 
-async function run_pulling(pulling_client) {
-    pulling_client.on('video', (videoData, timestamp) => {
+async function run_pulling() {
+    let pull_jupiter = new NodeRtmpEdgeChangeClient(
+        'rtmp://' + EDGE_JUPITER_IP + '/live/' + stream_key
+    );
+    pull_jupiter.on('video', (videoData, timestamp) => {
 
     });
-    pulling_client.startPull();
+    pull_jupiter.startPull();
 }
 
-run_pulling(pull_jupiter);
-
+setInterval(run_pulling, 2000);
+run_pulling();
 /*
 rtmp_polling_from_server_client.on('video', (videoData, timestamp) => {
     research_utils.appendLog(
