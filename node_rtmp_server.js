f25d47d71b11ad3bc2836afc2596edd8c4264fa3
Baekjun Kim <baekjun.kim@kaist.ac.kr>
2020-10-15 06:50:10 +0900

    add tls options to node_rtmp_server

--- node_rtmp_server.js
+++ node_rtmp_server.js
@@ -5,7 +5,17 @@
 //
 const Logger = require('./node_core_logger');
 
+const secure_mode = true;
 const Net = require('net');
+const fs = require('fs');
+const tls = require('tls');
+const options = {
+    key: fs.readFileSync('./server.pem'),
+    cert: fs.readFileSync('./server.crt')
+    //ca: fs.readFileSync('./csr.pem', 'utf8'),
+    //requestCert: false //do not request client side cert.
+} //tls key and cert.
+
 const NodeRtmpSession = require('./node_rtmp_session');
 const NodeCoreUtils = require('./node_core_utils');
 
@@ -16,15 +26,24 @@ const RTMP_PORT = 1935;
 class NodeRtmpServer {
   constructor(config) {
     config.rtmp.port = this.port = config.rtmp.port ? config.rtmp.port : RTMP_PORT;
-    this.tcpServer = Net.createServer((socket) => {
-      let session = new NodeRtmpSession(config, socket);
-      session.run();
-    })
-  }
+    if (secure_mode) { //may not working...
+        this.tcpServer = tls.createServer(options, (socket) => {
+        let session = new NodeRtmpSession(config, socket);
+        session.run();
+        })
+    }
+    else {
+        this.tcpServer = Net.createServer((socket) => {
+            let session = new NodeRtmpSession(config, socket);
+            session.run();
+        })
+    }}
 
   run() {
     this.tcpServer.listen(this.port, () => {
       Logger.log(`Node Media Rtmp Server started on port: ${this.port}`);
+      if (secure_mode) {
+          Logger.log('Secure mode ON')}
     });
 
     this.tcpServer.on('error', (e) => {
